e2ea27ac14b8ff05dc5b3e34bc41057129917bb9
lokideos <archmage09@gmail.com>
2021-03-03 20:36:03 +0300

    Setup basic Roda app boilerplate

---
+++ tasks/config/application.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Application < Roda
+  def self.root
+    File.expand_path('..', __dir__)
+  end
+
+  plugin :environments
+  plugin :json_parser
+  plugin(:not_found) { not_found_response }
+  include Errors
+
+  route do |r|
+    r.root do
+      response['Content-Type'] = 'application/json'
+      response.status = 200
+      { status: 'ok' }.to_json
+    end
+  end
+
+  private
+
+  def params
+    request.params
+  end
+end
