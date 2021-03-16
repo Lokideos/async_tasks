7643c21fdbc03b96ce5bebe0badeaf16f6be03cf
lokideos <archmage09@gmail.com>
2021-03-16 20:59:35 +0300

    177367489 | Fix user params contract

--- auth/app/contracts/users_contract.rb
+++ auth/app/contracts/users_contract.rb
@@ -4,6 +4,7 @@ class UsersContract < Dry::Validation::Contract
   schema do
     required(:name).filled(:string)
     required(:email).filled(:string)
+    required(:role).filled(:string)
     required(:password_digest).filled(:string)
   end
 end
