d0eb8880387fb6065c49e71acb348e0e90ed75cf
lokideos <archmage09@gmail.com>
2021-03-29 23:18:54 +0300

    177550801 | Change role column type to enum

--- auth/db/migrations/20210316204043_create_users.rb
+++ auth/db/migrations/20210316204043_create_users.rb
@@ -2,11 +2,15 @@
 
 Sequel.migration do
   up do
+    extension :pg_enum
+
+    create_enum(:user_roles_enum, %w'developer manager admin')
+
     create_table(:users) do
       primary_key :id, type: :Bignum
       column :name, 'character varying', null: false
       column :email, 'citext', null: false
-      column :role, 'character varying', null: false
+      column :role, 'user_roles_enum', null: false
       column :password_digest, 'character varying', null: false
       column :created_at, 'timestamp(6) without time zone', null: false
       column :updated_at, 'timestamp(6) without time zone', null: false
@@ -16,6 +20,9 @@ Sequel.migration do
   end
 
   down do
+    extension :pg_enum
+
     drop_table(:users)
+    drop_enum(:user_roles_enum)
   end
 end
--- auth/db/schema.rb
+++ auth/db/schema.rb
@@ -10,7 +10,7 @@ Sequel.migration do
       primary_key :id, :type=>:Bignum
       column :name, "character varying", :null=>false
       column :email, "citext", :null=>false
-      column :role, "character varying", :null=>false
+      column :role, "user_roles_enum", :null=>false
       column :password_digest, "character varying", :null=>false
       column :created_at, "timestamp(6) without time zone", :null=>false
       column :updated_at, "timestamp(6) without time zone", :null=>false
