527b124d9aca899c5a2987d26023743e140721db
lokideos <archmage09@gmail.com>
2021-06-27 20:55:18 +0300

    remove hot_reloader :(

--- auth/config/application_loader.rb
+++ auth/config/application_loader.rb
@@ -6,15 +6,7 @@ module ApplicationLoader
   def load_app!
     init_config
     init_db
-    if ENV['RACK_ENV'] == 'production'
-      require_app
-    else
-      loader = Zeitwerk::Loader.new
-      Dir["#{root}/app/**/*"].select { |f| File.directory?(f) }.each { |d| loader.push_dir(d) }
-      loader.push_dir("#{root}/app")
-      loader.push_dir("#{root}/config")
-      HotReloader.will_listen(loader)
-    end
+    require_app
     init_app
   end
 
